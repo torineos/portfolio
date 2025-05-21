98dbf52541966cc2c56c3573a6ea8a03303fc201
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-05-21 23:08:12 +0200

    feat: Responsive header depending on the screen size

---
+++ src/components/CollapsibleHiddenMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  
+  <div class="dropdown dropdown-end mr-2">
+    <div tabindex="0" role="button" class="btn">
+    <IconMenuDropdown  />
+    </div>
+
+      <ul
+        tabindex="0"
+        class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+      >
+        <li>
+          <RouterLink
+            :to="{ name: 'Skill' }"
+            aria-label="Skills page link"
+            @click.native="handleNativeClick"
+          >
+            Competences
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink
+            :to="{ name: 'CV' }"
+            aria-label="Online CV"
+            @click.native="handleNativeClick"
+          >
+            CV
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink
+            :to="{ name: 'About me' }"
+            aria-label="About me page link"
+            @click.native="handleNativeClick"
+          >
+            A propos
+          </RouterLink>
+        </li>
+
+        <li>
+          <RouterLink
+            :to="{ name: 'Projects' }"
+            aria-label="Projects page link"
+            @click.native="handleNativeClick"
+          >
+            Projets
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink
+            :to="{ name: 'Video en anglais' }"
+            aria-label="English video page link"
+            @click.native="handleNativeClick"
+          >
+            Video
+          </RouterLink>
+        </li>
+      </ul>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconMenuDropdown from "./icons/IconMenuDropdown.vue";
+
+function handleNativeClick(evt: Event) {
+  evt.stopPropagation();
+  evt.stopImmediatePropagation();
+  evt.preventDefault();
+}
+</script>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,70 +1,70 @@
 <template>
-  <nav>
-    <div>
+  <nav class="navbar bg-base-100 shadow-md shadow-primary/50">
+    <div class="navbar-start">
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'Home' }"
         aria-label="Home page link"
         @click.native="handleNativeClick"
       >
-        <button class="button-navbar">
-          Accueil
-        </button>
+        <button class="button-navbar">Accueil</button>
       </RouterLink>
-
+    </div>
+    <div class="navbar-center hidden ml-auto md:flex justify-end">
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'Skill' }"
         aria-label="Skills page link"
         @click.native="handleNativeClick"
       >
-        Competences
+        <button class="button-navbar">Competences</button>
       </RouterLink>
 
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'CV' }"
         aria-label="Online CV"
         @click.native="handleNativeClick"
       >
-        CV
+        <button class="button-navbar">CV</button>
       </RouterLink>
 
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'About me' }"
         aria-label="About me page link"
         @click.native="handleNativeClick"
       >
-        A propos
+        <button class="button-navbar">A propos</button>
       </RouterLink>
 
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'Projects' }"
         aria-label="Projects page link"
         @click.native="handleNativeClick"
       >
-        Projets
+        <button class="button-navbar">Projets</button>
       </RouterLink>
 
       <RouterLink
-        class="nav-link button-link about-link block hover:text-blue-400 dark:text-blue-600"
         :to="{ name: 'Video en anglais' }"
         aria-label="English video page link"
         @click.native="handleNativeClick"
       >
-        Video en anglais
+        <button class="button-navbar">Video</button>
       </RouterLink>
     </div>
-    <div>
-      <ThemeToggle />
+
+    <div class="navbar-end">
+      <div class="ml-auto flex md:hidden">
+        <CollapsibleHiddenMenu/>
+      </div>
+      <ThemeToggle class="pl-2" />
     </div>
+
   </nav>
+  
 </template>
 
 <script setup lang="ts">
 import ThemeToggle from "./ThemeToggle.vue";
+import CollapsibleHiddenMenu from "./CollapsibleHiddenMenu.vue";
 
 function handleNativeClick(evt: Event) {
   evt.stopPropagation();
