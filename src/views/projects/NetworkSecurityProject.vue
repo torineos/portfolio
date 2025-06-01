2fcaa4166c296002e71e16a8def5cfb47aa706f1
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-06-02 01:03:21 +0200

    feat: Component side navigation bar for projects created

--- src/views/projects/NetworkSecurityProject.vue
+++ src/views/projects/NetworkSecurityProject.vue
@@ -1,3 +1,75 @@
 <template>
+  <div
+    class="bg-[url(/miniat_multisites.webp)] bg-fixed h-24 mx-auto overflow-y-scroll border-x"
+  ></div>
 
+  <div class="grid grid-cols-3">
+    <ProjectsNavbar />
+    <div class="mx-10 mb-8 mt-5 col-span-2">
+      <p class="text-3xl text-center">Jouer et contrer une cyberattaque.</p>
+      <p class="text-2xl underline">Introduction:</p>
+      <p class="text-justify">
+        Ce projet est un travail en groupe. Nous devions réaliser une affiche en
+        groupe ayant comme objectif de sensibiliser des personnes en dehors du
+        domaine aux dangers informatiques.
+      </p>
+      <br />
+      <p class="text-2xl underline">Objectifs du projet:</p>
+      <ul class="list-image-[url(/src/components/icons/IconBook.vue)]">
+        <li>Travailler en groupe et se distribuer le travail.</li>
+        <li>
+          Gérer un site principal et un site secondaire dans un même domaine.
+        </li>
+        <li>Mettre en place une connexion VPN.</li>
+        <li>Suivre un cahier des charges.</li>
+      </ul>
+      <br />
+      <p class="text-2xl underline">Travail effectué:</p>
+      <p class="text-justify">
+        Ce projet était divisé en 3 parties, correspondant respectivement à 3
+        maquettes. A chaque début de maquette, nous avons réalisé un schéma
+        détaillé du réseau à mettre en place et réalisé un cahier des charges
+        avec les temps estimés de réalisation et le niveau de difficulté selon
+        nos aises.
+        <br />
+        La première maquette consistait à créer les 2 sites avec lequel on
+        allait installer notre infrastructure: un site principal et secondaire.
+        Dans ces 2 sites se trouvaient 2 serveurs Active Directory, chacun avec
+        des services différents. Nous avons également du gérer les GPO et les
+        autorisations d'utilisateurs itinérants sur certains dossiers.
+        <br />
+        La seconde maquette ne reprenait pas la structure de la première, et
+        consistait à créer un réseau interne derrière un Pare-feu/Routeur
+        OPNsense. Il fallait donc gérer les règles de pare-feu suffisamment
+        restrictives afin de garantir la sécurité du réseau et configurer la
+        traduction d'adresse IP en NAT afin de pouvoir accéder aux sites web
+        déployés sur les serveurs en interne. Enfin, nous avons du configurer
+        une connexion Vpn site-à-site IPSec.
+        <br />
+        La troisième maquette rassemblait les 2 premières, en installant les
+        équipements de la première maquette dans l'infrastructure de la seconde.
+        Un VPN nomade est configuré. A la fin du projet, il est possible après
+        une connexion par VPN d'accéder au réseau interne depuis l'extérieur.
+      </p>
+      <br />
+      <p class="text-2xl underline">Compétences acquises:</p>
+      <ul class="list-image-[url(/src/components/icons/IconBook.vue)]">
+        <li>Configurer des pare-feux restrictifs.</li>
+        <li>
+          Utiliser des règles de NAT afin de traduire les adresses IP entre les
+          réseaux internes et externes.
+        </li>
+        <li>
+          Mettre en place des réseaux privés gérés principalement par un seul
+          serveur Active Directory.
+        </li>
+      </ul>
+      <br />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import IconBook from "/src/components/icons/IconBook.vue";
+import ProjectsNavbar from "/src/components/ProjectsNavbar.vue";
+</script>
