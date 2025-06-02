1cda99581d4024913275abae8a7d0875f97cc4ba
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-06-02 04:26:31 +0200

    feat: Devweb page done

--- src/views/projects/DevWebProject.vue
+++ src/views/projects/DevWebProject.vue
@@ -2,70 +2,79 @@
   <div
     class="bg-[url(/miniat_devweb.webp)] bg-fixed h-24 mx-auto overflow-y-scroll border-x"
   ></div>
-  <div class="mx-20 mb-8 mt-5">
-    <p class="text-3xl text-center">Développement d'une application web</p>
-    <p class="text-2xl underline">Introduction:</p>
-    <p class="text-justify">
-      Ce projet est un travail en groupe. Nous devions réaliser une affiche en
-      groupe ayant comme objectif de sensibiliser des personnes en dehors du
-      domaine aux dangers informatiques.
-    </p>
-    <br />
-    <p class="text-2xl underline">Objectifs du projet:</p>
-    <ul class="list-image-[url(/src/components/icons/IconBook.vue)]">
-      <li>Créer une application qui répond au cahier des charges imposé.</li>
-      <li>
-        Manipuler des données dans une base de donnée (ajout, modification,
-        suppression).
-      </li>
-      <li>
-        Réaliser un site web dynamique, qui change en fonction du rôle de
-        l'utilisateur authentifié.
-      </li>
-    </ul>
-    <br />
-    <p class="text-2xl underline">Travail effectué:</p>
-    <p class="text-justify">
-      Ce projet est une mise en situation professionnelle pour réaliser une
-      application web correspondante aux demandes des clients (ici des
-      professeurs). Selon le projet qui nous était attribué, nous devions
-      proposer des solutions informatiques pour la gestion de personnel,
-      ressources humaines, ou des missions des personnels. Nous devions réaliser
-      le projet en binôme, donc nous devions nous répartir les tâches pour être
-      plus efficaces. Notre projet consistait à gérer les vacataires pour les
-      ressources humaines de l’IUT. Notre application devait s’adapter en
-      fonction de l’utilisateur connecté, et afficher /donner les permissions
-      les informations qui leurs sont nécessaires (qui changent en fonction du
-      rôle de l’utilisateur).
+  <div class="grid grid-cols-3">
+    <ProjectsNavbar />
+    <div class="mx-20 pt-6 mt-5 flex flex-col col-span-2">
+      <p class="text-3xl text-center">Développement d'une application web</p>
+      <p class="text-2xl underline pt-6">Introduction:</p>
+      <p class="text-justify">
+        Ce projet est un travail en binôme. Nous devions ici répondre à un
+        cahier des charges en Python + Flask pour le front-end afin de réaliser
+        une application web satisfaisante.
+      </p>
       <br />
-      Nous avons utilisé Pycharm (édition professionnelle) pour réaliser notre
-      application.
-    </p>
-    <br />
-    <p class="text-2xl underline">Compétences acquises:</p>
-    <ul class="list-image-[url(/src/components/icons/IconBook.vue)]">
-      <li>
-        Créer une application avec une interface compréhensible pour
-        l'utilisateur.
-      </li>
-      <li>
-        Manipuler des données dans une base de donnée (ajout, modification,
-        suppression).
-      </li>
-      <li>S'organiser en groupe pour se répartir les tâches.</li>
-      <li>
-        Se répartir les tâches équitablement dans un groupe pour être plus
-        efficaces.
-      </li>
-      <li>
-        Utiliser les ressources à notre disposition pour en tirer des
-        informations utiles.
-      </li>
-    </ul>
-    <br />
-    <p class="text-justify">
-      Notre affiche a fait parti des affiches désignées à être affichées dans le
-      hall RT !
-    </p>
+      <p class="text-2xl underline pt-6">Objectifs du projet:</p>
+      <ul class="list-disc">
+        <li>Créer une application qui répond au cahier des charges imposé.</li>
+        <li>
+          Manipuler des données dans une base de donnée (ajout, modification,
+          suppression).
+        </li>
+        <li>
+          Réaliser un site web dynamique, qui change en fonction du rôle de
+          l'utilisateur authentifié.
+        </li>
+      </ul>
+      <br />
+      <p class="text-2xl underline pt-6">Travail effectué:</p>
+      <p class="text-justify">
+        Ce projet est une mise en situation professionnelle pour réaliser une
+        application web correspondante aux demandes des clients (ici des
+        professeurs). Selon le projet qui nous était attribué, nous devions
+        proposer des solutions informatiques pour la gestion de personnel,
+        ressources humaines, ou des missions des personnels. Nous devions nous répartir les tâches
+        pour être plus efficaces. Notre projet consistait à gérer les vacataires
+        pour les ressources humaines de l’IUT. Notre application devait
+        s’adapter en fonction de l’utilisateur connecté, et afficher/donner les
+        permissions les informations qui leurs sont nécessaires (qui changent en
+        fonction du rôle de l’utilisateur).
+        <br />
+        <div class="columns-1xs items-center gap-8">
+        <img
+          class="object-contain md:object-cover text-center aspect-square rounded-xl"
+          src="/logged.webp"
+        />
+      </div>
+      <p class="text-xs text-center mb-8">Accueil d'un utilisateur ayant le rôle "vacataire".</p>
+        Nous avons utilisé Pycharm (édition professionnelle) pour réaliser notre
+        application.
+      </p>
+      <br />
+      <p class="text-2xl underline pt-6">Compétences acquises:</p>
+      <ul class="list-disc mb-8">
+        <li>
+          Créer une application avec une interface compréhensible pour
+          l'utilisateur.
+        </li>
+        <li>
+          Manipuler des données dans une base de donnée (ajout, modification,
+          suppression).
+        </li>
+        <li>S'organiser en groupe pour se répartir les tâches.</li>
+        <li>
+          Se répartir les tâches équitablement dans un groupe pour être plus
+          efficaces.
+        </li>
+        <li>
+          Utiliser les ressources à notre disposition pour en tirer des
+          informations utiles.
+        </li>
+      </ul>
+      <br />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ProjectsNavbar from "/src/components/ProjectsNavbar.vue";
+</script>
