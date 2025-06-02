485ed7b734c5c5b3e1b739221bc3e3745ee5ae22
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-06-02 06:45:28 +0200

    Rebuild packages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createI18n } from 'vue-i18n';
 import App from '@/App.vue';
 import router from "@/router";
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss(), lezer()],
+  base: '/portfolio/',  // Add this 
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
