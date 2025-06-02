46227cd3c2fd723e15e7d7e130f929ac905ce7c2
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-06-02 02:08:38 +0200

    feat: Competences and CV pages completed

--- src/components/SkillList.vue
+++ src/components/SkillList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul
-    class="menu rounded-box mx-auto divide-y divide-blue-200 grow rounded-box shadow-md"
+    class="menu rounded-box mx-auto divide-y divide-blue-200 grow rounded-box shadow-md mx-20 mb-8 mt-5"
   >
     <li class="flex">
       <div>
@@ -9,10 +9,11 @@
         <div>
           <div>Systèmes et applications</div>
           <div class="text-xs uppercase font-semibold opacity-60">
-            Remaining Reason
+            Administration et gestion des services
           </div>
         </div>
-        <div class="list-col-wrap text-s">
+        <div class="list-col-wrap">
+          <p>- Administration de postes Linux et Windows</p>
           <p>- Déploiement de services avec Docker-compose</p>
           <p>- Automatisation avec Ansible</p>
         </div>
@@ -23,11 +24,12 @@
         <div>
           <div>Réseaux</div>
           <div class="text-xs uppercase font-semibold opacity-60">
-            Remaining Reason
+            Configuration
           </div>
         </div>
-        <div class="list-col-wrap text-s">
-          <p>-</p>
+        <div class="list-col-wrap">
+          <p>- Configuration d'équipements L2/L3 et pare-feu</p>
+          <p>- Virtualisation avec Proxmox et VMware</p>
           <p>- Automatisation avec Ansible</p>
         </div>
       </div>
@@ -37,10 +39,10 @@
         <div>
           <div>Télécommunications</div>
           <div class="text-xs uppercase font-semibold opacity-60">
-            Remaining Reason
+            Compréhension des protocoles
           </div>
         </div>
-        <div class="list-col-wrap text-s">
+        <div class="list-col-wrap ">
           <p>- Téléphonie</p>
           <p>- Analyse du protocole NFC</p>
         </div>
@@ -54,8 +56,9 @@
             Languages de programmation
           </div>
         </div>
-        <div class="list-col-wrap text-s">
+        <div class="list-col-wrap ">
           <p>- Python, Java </p>
+          <p>- API REST, tests unitaires</p>
           <p>- Backend: Bases de données SQL Postgres</p>
           <p>- Web: HTML et CSS avec un framework javascript (Vue.js)</p>
         </div>
@@ -70,13 +73,14 @@
             Red et Blue team
           </div>
         </div>
-        <div class="list-col-wrap text-s">
-          <p>- Red team: Analyse de réseau avec Nmap, </p>
-          <p>- Blue team: Hardening Windows et Linux, Mise en place d'un SIEM (ELK), Solutions de backup (Zabbix)</p>
+        <div class="list-col-wrap ">
+          <p>- Red team: Analyse de réseau avec Nmap, Fuzzing de site web avec Dirbuster, utilisation de CVE avec Metasploit...</p>
+          <p>- Blue team: Hardening Windows et Linux, Mise en place d'un SIEM (ELK), Solutions de backup (Zabbix), Supervision (PRTG)</p>
         </div>
       </div>
     </li>
   </ul>
+  <br/>
 </template>
 
 <script setup lang="ts">
--- src/views/SkillView.vue
+++ src/views/SkillView.vue
@@ -2,22 +2,6 @@
   
 <SkillList />
    
-  <p class="text-base-content rounded p-10 text-center">Compétences</p>
-
-  Compétences techniques Utiliser et modéliser des modèles de Gantt
-  Programmation en Python sous Pycharm Administrer un ordinateur sous Windows
-  Utiliser les commandes Shell via un terminal Linux Création de pages Web
-  (HTML, CSS, Bootstrap) Configurer un pare-feu et routeur Cisco Créer et gérer
-  un réseau multi-sites.
-  <p>Systèmes et applications</p>
-  <p>Développement Python Scripting bash</p>
-  <p>Réseaux Configuration de switchs, routeurs, firewalls Cablâge réseau</p>
-  <p>
-    <IconShield />
-    Cybersécurité Red Team: - Reconnaissance réseau Nmap - Mimikatz - Kali Linux
-    Blue Team: - Hardening Linux et Windows
-  </p>
-  <p>Telecommunications Téléphonie Analyse du protocole NFC</p>
 </template>
 
 <script setup lang="ts">
