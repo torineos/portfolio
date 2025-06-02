019907bb4f879763c322c53bf9f0eb386d225f91
MATHIOT Makoto <noemie.mathiot@etu.univ-grenoble-alpes.fr>
2025-06-02 04:33:04 +0200

    feat: multisite page done

--- src/views/projects/MultisiteProject.vue
+++ src/views/projects/MultisiteProject.vue
@@ -5,9 +5,9 @@
 
   <div class="grid grid-cols-3">
     <ProjectsNavbar />
-    <div class="mx-10 mb-8 mt-5 col-span-2">
+    <div class="mx-10 mb-8 mt-5 flex flex-col col-span-2">
       <p class="text-3xl text-center">Conception d'un réseau multi-sites</p>
-      <p class="text-2xl underline">Introduction:</p>
+      <p class="text-2xl underline pt-6">Introduction:</p>
       <p class="text-justify">
         Ce projet est un travail en binôme. Ici le but est de travailler avec
         des réseaux que l'on peut typiquement retrouver en entreprise avec
@@ -17,7 +17,7 @@
         les serveurs.
       </p>
       <br />
-      <p class="text-2xl underline">Objectifs du projet:</p>
+      <p class="text-2xl underline pt-6">Objectifs du projet:</p>
       <ul class="list-disc">
         <li>Travailler en groupe et se distribuer le travail.</li>
         <li>
@@ -27,7 +27,7 @@
         <li>Suivre un cahier des charges.</li>
       </ul>
       <br />
-      <p class="text-2xl underline">Travail effectué:</p>
+      <p class="text-2xl underline pt-6">Travail effectué:</p>
       <p class="text-justify">
         Ce projet est divisé en 3 parties, correspondant respectivement à 3
         maquettes.<br />
@@ -42,6 +42,14 @@
         des services différents. Nous avons également du gérer les GPO et les
         autorisations d'utilisateurs itinérants sur certains dossiers.
         <br />
+        <div class="columns-1xs items-center gap-8">
+        <img
+          class="object-contain md:object-cover text-center aspect-square rounded-xl"
+          src="/multisite_sh2.webp"
+        />
+      </div>
+      <p class="text-xs text-center mb-8">Second schéma du projet.</p>
+
         La seconde maquette ne reprenait pas la structure de la première, et
         consistait à créer un réseau interne derrière un Pare-feu/Routeur
         OPNsense. Il fallait donc gérer les règles de pare-feu suffisamment
@@ -50,13 +58,21 @@
         déployés sur les serveurs en interne. Enfin, nous avons du configurer
         une connexion Vpn site-à-site IPSec.
         <br />
+        <div class="columns-1xs items-center gap-8">
+        <img
+          class="object-contain md:object-cover text-center aspect-square rounded-xl"
+          src="/multisite_sh3.webp"
+        />
+      </div>
+      <p class="text-xs text-center mb-8">Dernier schéma du projet, la fusion
+						de la 1ere et 2eme maquette.</p>
         La troisième maquette rassemblait les 2 premières, en installant les
         équipements de la première maquette dans l'infrastructure de la seconde.
         Un VPN nomade est configuré. A la fin du projet, il est possible après
         une connexion par VPN d'accéder au réseau interne depuis l'extérieur.
       </p>
       <br />
-      <p class="text-2xl underline">Compétences acquises:</p>
+      <p class="text-2xl underline pt-6">Compétences acquises:</p>
       <ul class="list-disc">
         <li>Configurer des pare-feux restrictifs.</li>
         <li>
